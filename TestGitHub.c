26285813b4efb5188a58d7f7c25f5afa16208313
GrigoreStefanD <stefangrigore07@gmail.com>
2025-03-07 11:54:29 +0200

    Add project files.

---
+++ TestGitHub.c
@@ -0,0 +1,12 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() 
+{
+    char nume[50];
+    printf("tasteaza  numele tau  : ");
+    scanf("%s", nume);
+    printf("salut %s! bine ai venit!\n", nume);
+    return 0;
+}
