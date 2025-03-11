204c270b242249652bdd38bb4963b49fdd68c547
GrigoreStefanD <stefangrigore07@gmail.com>
2025-03-11 14:18:06 +0200

    structura pentru un animal

--- Ferma.c
+++ Ferma.c
@@ -12,28 +12,12 @@ struct Animal
 {
     char nume[50];
     int varsta;
-    struct Animal* urmator; // pentru lista simpl?
-    struct Animal* precedent; // pentru lista dubl?
+    struct Animal* urmator; 
+    struct Animal* precedent; 
 };
-
-
-struct Animal* creareAnimal(char* nume, int varsta) 
-{
-    struct Animal* animal = (struct Animal*)malloc(sizeof(struct Animal));
-    strcpy(animal->nume, nume);
-    animal->varsta = varsta;
-    animal->urmator = NULL;
-    animal->precedent = NULL;
-    return animal;
-
-}
-
-
-
-
 
 int main()
 {
 
-	return 0;
+    return 0;
 }
