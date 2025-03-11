48169f150825ff6c4e209bede05103f6e41d4ee1
GrigoreStefanD <stefangrigore07@gmail.com>
2025-03-11 13:46:03 +0200

    structura pentru un animal

---
+++ Ferma.c
@@ -0,0 +1,23 @@
+
+
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+struct Animal
+{
+    char nume[50];
+    int varsta;
+    struct Animal* urmator; // pentru lista simpl?
+    struct Animal* precedent; // pentru lista dubl?
+};
+
+int main()
+{
+
+	return 0;
+}
