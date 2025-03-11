335ffacd3cd7255740b5d584506b096dabbddeb9
GrigoreStefanD <stefangrigore07@gmail.com>
2025-03-11 14:19:35 +0200

    afisare animal din lista simpla

--- Ferma.c
+++ Ferma.c
@@ -1,5 +1,6 @@
 
 
+
 #define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
@@ -12,9 +13,33 @@ struct Animal
 {
     char nume[50];
     int varsta;
+
     struct Animal* urmator; 
-    struct Animal* precedent; 
+    struct Animal* precedent;
 };
+
+struct Animal* creareAnimal(char* nume, int varsta)
+{
+    struct Animal* animal = (struct Animal*)malloc(sizeof(struct Animal));
+    strcpy(animal->nume, nume);
+    animal->varsta = varsta;
+    animal->urmator = NULL;
+    animal->precedent = NULL;
+    return animal;
+}
+
+void afisareListaSimpla(struct Animal* cap)
+{
+    struct Animal* curent = cap;
+    while (curent != NULL)
+    {
+        printf("nume: %s, varsta: %d\n", curent->nume, curent->varsta);
+        curent = curent->urmator;
+    }
+}
+
+
+
 
 int main()
 {
