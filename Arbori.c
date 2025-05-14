9c0fbc0957f1867b97f66211601644a7c427ded7
GrigoreStefanD <stefangrigore07@gmail.com>
2025-05-14 11:09:04 +0300

    comentat

--- Arbori.c
+++ Arbori.c
@@ -1,68 +1,68 @@
-#define  _CRT_SECURE_NO_WARNINGS
-
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-
-struct Student {
-    char nume[50];
-    struct Student* stanga, * dreapta;
-};
-
-
-
-
-
-struct Student* creeazaStudent(char nume[]) {
-    struct Student* nou = (struct Student*)malloc(sizeof(struct Student));
-    strcpy(nou->nume, nume);
-    nou->stanga = nou->dreapta = NULL;
-    return nou;
-}
-
-
-
-
-struct Student* insereaza(struct Student* radacina, char nume[]) 
-{
-    if (radacina == NULL)
-        return creeazaStudent(nume);
-    if (strcmp(nume, radacina->nume) < 0)
-        radacina->stanga = insereaza(radacina->stanga, nume);
-    else if (strcmp(nume, radacina->nume) > 0)
-        radacina->dreapta = insereaza(radacina->dreapta, nume);
-    return radacina;
-}
-
-
-
-
-void inordine(struct Student* radacina) 
-{
-    if (radacina != NULL) {
-        inordine(radacina->stanga);
-        printf("%s ", radacina->nume);
-        inordine(radacina->dreapta);
-    }
-}
-
-int main() 
-{
-    struct Student* radacina = NULL;
-    radacina = insereaza(radacina, "Ana");
-    insereaza(radacina, "Bogdan");
-    insereaza(radacina, "Cristina");
-    insereaza(radacina, "David");
-
-
-    printf("Studenti ordonati: ");
-
-    inordine(radacina);
-    printf("\n");
-
-    return 0;
-}
+//#define  _CRT_SECURE_NO_WARNINGS
+//
+//
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//
+//
+//struct Student {
+//    char nume[50];
+//    struct Student* stanga, * dreapta;
+//};
+//
+//
+//
+//
+//
+//struct Student* creeazaStudent(char nume[]) {
+//    struct Student* nou = (struct Student*)malloc(sizeof(struct Student));
+//    strcpy(nou->nume, nume);
+//    nou->stanga = nou->dreapta = NULL;
+//    return nou;
+//}
+//
+//
+//
+//
+//struct Student* insereaza(struct Student* radacina, char nume[]) 
+//{
+//    if (radacina == NULL)
+//        return creeazaStudent(nume);
+//    if (strcmp(nume, radacina->nume) < 0)
+//        radacina->stanga = insereaza(radacina->stanga, nume);
+//    else if (strcmp(nume, radacina->nume) > 0)
+//        radacina->dreapta = insereaza(radacina->dreapta, nume);
+//    return radacina;
+//}
+//
+//
+//
+//
+//void inordine(struct Student* radacina) 
+//{
+//    if (radacina != NULL) {
+//        inordine(radacina->stanga);
+//        printf("%s ", radacina->nume);
+//        inordine(radacina->dreapta);
+//    }
+//}
+//
+//int main() 
+//{
+//    struct Student* radacina = NULL;
+//    radacina = insereaza(radacina, "Ana");
+//    insereaza(radacina, "Bogdan");
+//    insereaza(radacina, "Cristina");
+//    insereaza(radacina, "David");
+//
+//
+//    printf("Studenti ordonati: ");
+//
+//    inordine(radacina);
+//    printf("\n");
+//
+//    return 0;
+//}
