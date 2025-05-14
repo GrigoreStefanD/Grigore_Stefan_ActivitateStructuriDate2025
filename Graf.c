17ccff2a82b8eefe7bc170f8f4d66e883f80be02
GrigoreStefanD <stefangrigore07@gmail.com>
2025-05-14 13:02:00 +0300

    graf com

--- Graf.c
+++ Graf.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
-
-#define V 5
-
-struct Graf {
-    int matrice[V][V];
-};
-
-void afiseazaMatrice(struct Graf* g) {
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++)
-            printf("%d ", g->matrice[i][j]);
-        printf("\n");
-    }
-}
-
-int main() {
-    struct Graf librarie = { {{0, 1, 0, 0, 1},
-                             {1, 0, 1, 1, 0},
-                             {0, 1, 0, 1, 1},
-                             {0, 1, 1, 0, 1},
-                             {1, 0, 1, 1, 0}} };
-
-    printf("Relatii autori-carti:\n");
-    afiseazaMatrice(&librarie);
-    return 0;
-}
+//#include <stdio.h>
+//
+//#define V 5
+//
+//struct Graf {
+//    int matrice[V][V];
+//};
+//
+//void afiseazaMatrice(struct Graf* g) {
+//    for (int i = 0; i < V; i++) {
+//        for (int j = 0; j < V; j++)
+//            printf("%d ", g->matrice[i][j]);
+//        printf("\n");
+//    }
+//}
+//
+//int main() {
+//    struct Graf librarie = { {{0, 1, 0, 0, 1},
+//                             {1, 0, 1, 1, 0},
+//                             {0, 1, 0, 1, 1},
+//                             {0, 1, 1, 0, 1},
+//                             {1, 0, 1, 1, 0}} };
+//
+//    printf("Relatii autori-carti:\n");
+//    afiseazaMatrice(&librarie);
+//    return 0;
+//}
